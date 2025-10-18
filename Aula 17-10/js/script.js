a7abb3d2db24da049352bb6e098421651930a008
Felipe Trindade <felipe_biel3@hotmail.com>
2025-10-17 21:30:04 -0300

    aula dia 17-10

---
+++ Aula 17-10/js/script.js
@@ -0,0 +1,10 @@
+let paragrafo = document.getElementById('nome');
+function exibir(){
+    console.log(paragrafo);
+    paragrafo.innerHTML = 'Felipe Trindade!'
+}
+function ocult(){
+    
+    document.getElementById('nome');
+    paragrafo.innerHTML = '';
+}
